587129a7caa6403ee2bea1ee8d202c2ba17ca061
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-01 23:32:10 +0200

    styling navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import NavigationBar from './components/NavigationBar';
-// import styles from './App.module.css'
+import styles from './App.module.css'
 
 const App: React.FC = () => {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <NavigationBar />
       </header>
     </>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,73 +2,80 @@ import styles from './NavigationBar.module.css'
 
 const NavigationBar: React.FC = () => {
   return (
-    <nav className={styles.nav_bar}>
-      <div>
-        <a href=''>
-          <img
-            className={styles.nav_bar__logo}
-            src={require('../images/logo-one.png')}
-            alt='logo'
-          />
-        </a>
-      </div>
-      <ul className={styles.nav_bar__lists}>
-        <li>
-          <a href=''>O mnie</a>
-        </li>
-        <li>
-          <a href=''>Portfolio</a>
-        </li>
-        <li>
-          <a href=''>Działalność</a>
-        </li>
-        <li>
-          <a href=''>Kontakt</a>
-        </li>
-      </ul>
-      <ul className={styles.nav_bar__lists}>
-        <li>+ 531 767 911</li>
-      </ul>
-      <ul className={styles.nav_bar__lists}>
-        <li>
-          <a href=''>insta ikonka</a>
-        </li>
-        <li>
-          <a href=''>email ikonka</a>
-        </li>
-      </ul>
-
-      {/* <button class='hamburger hamburger--collapse' type='button'>
-          <span class='hamburger-box'>
-            <span class='hamburger-inner'></span>
-          </span>
-        </button> */}
-    </nav>
-
-  /* <div class='hero-container'>
-        <div class='wrapper one'>
-          <h1 class='wrapper__header'>
-            <span class='wrapper__header wrapper__header-beauty'>Beauty</span>
-            <span class='wrapper__header wrapper__header-logic'>Logic</span>
-          </h1>
-          <h2 class='wrapper__subheader'>Agata Pulchny - Ambroziak</h2>
-          <p class='wrapper__text'>
-            Odkryj piękno swojej natury z Beauty Logic. Nasz zespół specjalistów
-            dąży do tego, aby pomóc Ci poczuć się jak najlepiej. Zaufaj Beauty
-            Logic i pozwól nam wyjąć to, co najlepsze w Tobie.
-          </p>
-          <a class='button-link' href='#offers'>
-            <span class='wrapper__button'>Sprawdź Ofertę</span>
+    <>
+      <nav className={styles.nav_bar}>
+        <div className={styles.logo_container}>
+          <a href='#home'>
+            <img
+              className={styles.logo_container__image}
+              src={require('../images/logo-two.png')}
+              alt='logo'
+            />
           </a>
         </div>
-        <div class='wrapper two'>
-          <img
-            class='wrapper__image'
-            src='./src/img/hero-img.png'
-            alt='woman'
-          />
-        </div>
-      </div> */
+        <ul className={styles.nav_bar__list}>
+          <li className={styles.nav_bar__list_item}>
+            <a href=''>O mnie</a>
+          </li>
+          <li className={styles.nav_bar__list_item}>
+            <a href=''>Portfolio</a>
+          </li>
+          <li className={styles.nav_bar__list_item}>
+            <a href=''>Działalność</a>
+          </li>
+          <li className={styles.nav_bar__list_item}>
+            <a href=''>Kontakt</a>
+          </li>
+        </ul>
+        <ul className={styles.nav_bar__social}>
+          <li className={styles.nav_bar__social_item}>
+            <a className={styles.icons} href=''>
+              I
+            </a>
+          </li>
+          <li className={styles.nav_bar__social_item}>
+            <a className={styles.icons} href=''>
+              E
+            </a>
+          </li>
+          <li className={styles.nav_bar__social_item}>
+            <a className={styles.icons} href=''>
+              T
+            </a>
+          </li>
+        </ul>
+
+        {/* <button class='hamburger hamburger--collapse' type='button'>
+        <span class='hamburger-box'>
+          <span class='hamburger-inner'></span>
+        </span>
+      </button> */}
+      </nav>
+
+      {/* <div class='hero-container'>
+              <div class='wrapper one'>
+                  <h1 class='wrapper__header'>
+                      <span class='wrapper__header wrapper__header-beauty'>Beauty</span>
+                      <span class='wrapper__header wrapper__header-logic'>Logic</span>
+                  </h1>
+                  <h2 class='wrapper__subheader'>Agata Pulchny - Ambroziak</h2>
+                  <p class='wrapper__text'>
+                      Odkryj piękno swojej natury z Beauty Logic. Nasz zespół specjalistów
+                      dąży do tego, aby pomóc Ci poczuć się jak najlepiej. Zaufaj Beauty
+                      Logic i pozwól nam wyjąć to, co najlepsze w Tobie.
+                  </p>
+                  <a class='button-link' href='#offers'>
+                      <span class='wrapper__button'>Sprawdź Ofertę</span>
+                  </a>
+              </div>
+              <div class='wrapper two'>
+                  <img
+                      class='wrapper__image'
+                      src='./src/img/hero-img.png'
+                      alt='woman' />
+              </div>
+          </div> */}
+    </>
   )
 }
 
