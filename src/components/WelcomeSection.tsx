62b98c466d0b7bff8c284598a8396c57e0444ac9
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-07 01:18:45 +0200

    adding more text and chaning font family

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -6,17 +6,21 @@ const WelcomeSection: React.FC = () => {
     <section>
       <div className={styles.welcome_section}>
         <div className={styles.welcome_header__container}>
-          <p className={styles.welcome_text}>#interior, #home, #passion</p>
+          <p className={styles.welcome_text__top}>#interior, #home, #passion</p>
           <h1 className={styles.welcome_header}>
             Let&apos;s <span className={styles.header_italic}> Make</span> Your
             Interior <span className={styles.header_italic}>Better</span>
           </h1>
+          <p className={styles.welcome_text__bottom}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            fugiat fugit quae in voluptatum rerum aliquid sequi corporis
+          </p>
         </div>
         <div className={styles.welcome_image__container}>
           <picture>
             <source
               srcSet={require('../images/stowidokow3-logo-half.png')}
-              media='(max-width: 1639px)'
+              media='(max-width: 1344px)'
             />
             <img
               src={require('../images/stowidokow3-logo.png')}
