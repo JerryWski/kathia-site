5ced3c9f2435e3b7e273a4a52ed35e4835bdf537
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-07 14:49:55 +0200

    adding arrow + some changes

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -6,15 +6,11 @@ const WelcomeSection: React.FC = () => {
     <section>
       <div className={styles.welcome_section}>
         <div className={styles.welcome_header__container}>
-          <p className={styles.welcome_text__top}>#interior, #home, #passion</p>
+          <p className={styles.welcome_text__top}>#interior, #home, #design</p>
           <h1 className={styles.welcome_header}>
             Let&apos;s <span className={styles.header_italic}> Make</span> Your
             Interior <span className={styles.header_italic}>Better</span>
           </h1>
-          <p className={styles.welcome_text__bottom}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            fugiat fugit quae in voluptatum rerum aliquid sequi corporis
-          </p>
         </div>
         <div className={styles.welcome_image__container}>
           <picture>
@@ -34,6 +30,12 @@ const WelcomeSection: React.FC = () => {
             className={styles.test}
           /> */}
         </div>
+        <p className={styles.welcome_text__bottom}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit Culpa
+        </p>
+        <a className={styles.link_arrow} href=''>
+          <i className='fa-solid fa-chevron-down fa-2x'></i>
+        </a>
       </div>
     </section>
   );
