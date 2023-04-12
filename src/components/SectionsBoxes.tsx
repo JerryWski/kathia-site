2ae0ea8c90b05dd9eafdb5db3330cf1773b98cbd
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-13 00:54:57 +0200

    testing boxes layout

--- src/components/SectionsBoxes.tsx
+++ src/components/SectionsBoxes.tsx
@@ -3,17 +3,29 @@ import styles from './SectionBoxes.module.css';
 const SectionBoxes: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-        <h2 className={styles.boxes_header}>Poznajmy się lepiej</h2>
-        <span className={styles.header_underline}></span>
+      <h2 className={styles.boxes_header}>Poznajmy się lepiej</h2>
+      <span className={styles.header_underline}></span>
       <div className={styles.boxes_container}>
         <div className={styles.box_one}>
-          <a className={styles.box_one__link} href=''>O mnie</a>
+          <div className={styles.box_border}></div>
+          <img src={require('../images/coffie-woman3.png')}></img>
+          <a className={styles.box_one__link} href=''>
+            O Mnie
+          </a>
         </div>
         <div className={styles.box_two}>
-          <a className={styles.box_two__link} href=''>Portfolio</a>
+          <div className={styles.box_border}></div>
+          <img src={require('../images/coffie-woman2.png')}></img>
+          <a className={styles.box_two__link} href=''>
+            Portfolio
+          </a>
         </div>
         <div className={styles.box_three}>
-          <a className={styles.box_three__link} href=''>Działalność</a>
+          <div className={styles.box_border}></div>
+          <img className={styles.coop} src={require('../images/co-op.png')}></img>
+          <a className={styles.box_three__link} href=''>
+            Działalność
+          </a>
         </div>
       </div>
     </div>
