c1b6508eca4d6376841f9a92261302ebc101456f
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-14 21:53:16 +0200

    fixing deployment

--- src/components/SectionsBoxes.tsx
+++ src/components/SectionsBoxes.tsx
@@ -3,14 +3,17 @@ import styles from './SectionBoxes.module.css';
 const SectionBoxes: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.boxes_header}>Poznajmy się lepiej</h2>
+      <h2 className={styles.boxes_header}>
+        Poznajmy <span className={styles.boxes_header__span}>się</span> lepiej
+      </h2>
       <span className={styles.header_underline}></span>
       <div className={styles.boxes_container}>
         <div className={styles.box_one}>
           <div className={styles.box_border}></div>
           <img
             className={styles.box_image}
-            src={require('../images/coffie.png')} alt='kitchen-accessories'
+            src={require('../images/coffie.png')}
+            alt='kitchen-accessories'
           ></img>
           <a className={styles.box_one__link} href=''>
             O Mnie
@@ -20,7 +23,8 @@ const SectionBoxes: React.FC = () => {
           <div className={styles.box_border}></div>
           <img
             className={styles.box_image}
-            src={require('../images/coffie.png')} alt='kitchen-accessories'
+            src={require('../images/coffie.png')}
+            alt='kitchen-accessories'
           ></img>
           <a className={styles.box_two__link} href=''>
             Portfolio
