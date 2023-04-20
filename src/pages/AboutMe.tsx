26c08362a37714a6749ef3feded89c4bb9199943
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-20 21:32:25 +0200

    about me subpage

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,24 +6,39 @@ const AboutMe: React.FC = () => {
       <div className={styles.about_wrapper}>
         <div className={styles.intro_container}>
           <p className={styles.intro_text}>
-            &apos; Poznajmy się! dlatego teraz trochę... &apos;
+            &apos; Teraz <span className={styles.intro_text__about}>Parę</span>{' '}
+            słów... &apos;
           </p>
           <h2 className={styles.intro_heading}>o Mnie</h2>
         </div>
         <div className={styles.image_container}>
           <img
             className={styles.profile_image}
-            src={require('../images/woman-test.jpg')}
+            src={require('../images/kathia.png')}
             alt='woman'
           ></img>
         </div>
+        <div className={styles.description_container}>
+          <p className={styles.bio_descritpion}>
+            Kasia. Prowadzi konto na Instagramie o nazwie @sto_widokow.home
+            Miłośniczka kawy i przytulnych wnętrz. Choć w życiu zawodowym
+            zajmowała się głównie rekrutacją, sprzedażą i szkoleniami, to pasja
+            do wnętrz i fotografii sprawiła, że prowadzenie tego profilu sprawia
+            jej najwięcej radości.
+          </p>
+          <p className={styles.bio_descritpion}>
+            Uczestniczyła w szkoleniach z social mediów, nadzorowała od strony
+            marketingowej pracę dwóch sklepów internetowych, dzięki czemu nabyła
+            kompetencje z zakresu influencer marketingu. Udziela na swoim koncie
+            porad poszerzających wiedzę o Instagramie i z zakresu rozwijania
+            profilu wnętrzarskiego.
+          </p>
+          <p className={styles.bio_descritpion}>
+            Jej marzeniem od zawsze było napisać własną książkę, póki co wydała
+            e-booka i ma apetyt na więcej!
+          </p>
+        </div>
       </div>
-      <p className={styles.intro_descritpion}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut minus
-        praesentium molestias officiis accusantium aliquid quo perferendis ex.
-        Quidem fuga, voluptas maiores nesciunt cupiditate est vitae quaerat
-        deserunt dignissimos eveniet!
-      </p>
     </section>
   );
 };
