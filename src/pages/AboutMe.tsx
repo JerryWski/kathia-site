e770f1bd03bf28c5e1a5f5481cc265b97c61c0ec
JerryWski <jarek_wasilewski@yahoo.com>
2023-04-24 21:40:08 +0200

    more styling plus some schanges for about me

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,8 +1,9 @@
 import styles from './AboutMe.module.css';
+import ContactSection from '../components/ContactSection';
 
 const AboutMe: React.FC = () => {
   return (
-    <section id='about-me'>
+    <section id='about-me' className={styles.about_section}>
       <div className={styles.about_wrapper}>
         <div className={styles.intro_container}>
           <h2 className={styles.intro_heading}>O Mnie</h2>
@@ -14,13 +15,18 @@ const AboutMe: React.FC = () => {
             alt='woman'
           ></img>
         </div>
-          {/* <span className={styles.test}>Śledź</span> */}
+        {/* <span className={styles.test}>Śledź</span> */}
         <div className={styles.description_container}>
           <p className={styles.bio_descritpion}>Cześć!</p>
           <p className={styles.bio_descritpion}>
-            Kasia. Prowadzi konto na Instagramie o nazwie
-            <a className={styles.bio_link} href='https://www.instagram.com/stowidokow.home/'>@sto_widokow.home</a>.
-            Miłośniczka kawy i przytulnych wnętrz. Choć w życiu zawodowym
+            Jestem Kasia. Prowadzę konto na Instagramie o nazwie
+            <a
+              className={styles.bio_link}
+              href='https://www.instagram.com/stowidokow.home/'
+            >
+              @stowidokow.home
+            </a>
+            . Miłośniczka kawy i przytulnych wnętrz. Choć w życiu zawodowym
             zajmowała się głównie rekrutacją, sprzedażą i szkoleniami, to pasja
             do wnętrz i fotografii sprawiła, że prowadzenie tego profilu sprawia
             jej najwięcej radości.
@@ -34,10 +40,15 @@ const AboutMe: React.FC = () => {
           </p>
           <p className={styles.bio_descritpion}>
             Jej marzeniem od zawsze było napisać własną książkę, póki co wydała
-            <a className={styles.bio_link} href=""> e-booka</a> i ma apetyt na więcej!
+            <a className={styles.bio_link} href=''>
+              {' '}
+              e-booka
+            </a>{' '}
+            i ma apetyt na więcej!
           </p>
         </div>
       </div>
+      <ContactSection/>
     </section>
   );
 };
