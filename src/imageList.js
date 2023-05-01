3ac40f22dbf058168400081de605e0d2961c88e3
JerryWski <jarek_wasilewski@yahoo.com>
2023-05-02 00:55:35 +0200

    creating li based on map method, different approach

---
+++ src/imageList.js
@@ -0,0 +1,14 @@
+export const imageList = [
+  {
+    id: 1,
+    image: require('./images/fakro_test.png'),
+    title: 'Zdjęcie 1',
+    text: 'To jest opis pierwszego zdjecia'
+  },
+  {
+    id: 2,
+    image: require('./images/tagu_test.png'),
+    title: 'Zdjęcie 2',
+    text: 'To jest opis drugiego zdjecia'
+  }
+];
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,17 +1,35 @@
 import ContactSection from '../components/ContactSection';
 import styles from './Portfolio.module.css';
+import { imageList } from '../imageList';
 
 const Portfolio: React.FC = () => {
   return (
-    <div id='portfolio' className={styles.portfolio_section}>
+    <section id='portfolio' className={styles.portfolio_section}>
       <div className={styles.portfolio_wrapper}>
         <div className={styles.intro_container}>
           <h2 className={styles.intro_heading}>Portfolio</h2>
+          <p className={styles.intro_description}>
+            dsadsadsdasdasdadasdadasdasd
+          </p>
         </div>
-
+        <div className={styles.portfolio_lists__container}>
+          <ul className={styles.portfolio_lists}>
+            {imageList.map((item) => (
+              <li key={item.id} className={styles.portfolio_item}>
+                <a href=''>
+                  <div className={styles.portfolio_description}>
+                    <h3 className={styles.portfolio_title}>{item.title}</h3>
+                    <p className={styles.portfolio_text}>Something about</p>
+                    <img src={item.image}></img>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <ContactSection />
-    </div>
+    </section>
   );
 };
 
