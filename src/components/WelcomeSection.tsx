1ce8e386c819ba7cce7b40b9d3c5de22fb64b844
JerryWski <jarek_wasilewski@yahoo.com>
2023-05-28 12:18:17 +0200

    adding more images and styling

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -7,40 +7,62 @@ const WelcomeSection: React.FC = () => {
       <div className={styles.welcome_section}>
         <div className={styles.welcome_header__container}>
           <p className={styles.welcome_text__top}>wnętrza, dom, design</p>
-          <h1 className={styles.welcome_header}>
-            Marka Własna
-          </h1>
+          <h1 className={styles.welcome_header}>Marka Własna</h1>
         </div>
         <div className={styles.welcome_image__container}>
+
           <div className={styles.slider_wrapper}>
             <picture>
               <source
-                srcSet={require('../images/stowidokow3-logo-half.png')}
+                srcSet={require('../images/stowidokow-logo-half.png')}
                 media='(max-width: 1339px)'
               />
               <img
-                src={require('../images/stowidokow3-logo.png')}
+                src={require('../images/stowidokow-logo.png')}
                 alt='brand-logo'
                 className={styles.welcome_image}
               />
             </picture>
           </div>
           <div className={styles.slider_wrapper}>
-            {/* trzeba bedzie zmienić na końcu na sam tag img */}
-            <picture>
-              <img
-                src={require('../images/insta-image.png')}
-                alt='interior-house'
-                className={styles.welcome_image}
-              />
-            </picture>
+            <img
+              src={require('../images/insta-image.jpg')}
+              alt='interior-house'
+              className={styles.welcome_image}
+            />
           </div>
+          <div className={styles.slider_wrapper}>
+            <img
+              src={require('../images/baby-room.jpg')}
+              alt='baby-room'
+              className={styles.welcome_image}
+            />
+          </div>
+          <div className={styles.slider_wrapper}>
+            <img
+              src={require('../images/bathroom.jpg')}
+              alt='bathroom'
+              className={styles.welcome_image}
+            />
+          </div>
+          <div className={styles.slider_wrapper}>
+            <img
+              src={require('../images/bedroom2.jpg')}
+              alt='bedroom'
+              className={styles.welcome_image}
+            />
+          </div>
+
         </div>
 
         <p className={styles.welcome_text__bottom}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit Culpa
         </p>
-        <a className={styles.link_arrow} href='#home'aria-label='arrow link to scroll down'>
+        <a
+          className={styles.link_arrow}
+          href='#home'
+          aria-label='arrow link to scroll down'
+        >
           <i className='fa-solid fa-chevron-down fa-2x'></i>
         </a>
       </div>
