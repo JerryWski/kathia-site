448195a24b956bf190afc642734da707ce844990
JerryWski <jarek_wasilewski@yahoo.com>
2023-07-25 20:34:10 +0200

    fixing links

--- src/imageList.js
+++ src/imageList.js
@@ -4,35 +4,35 @@ export const imageList = [
     image: require('./images/fakro.png'),
     title: 'Fakro',
     text: 'Promocja rolet światowej marki branży budowlanej',
-    link: 'https://shorturl.at/rFJX4'
+    link: 'https://www.instagram.com/reel/Chl1Ub0jhEc/?igshid=YmMyMTA2M2Y%3D'
   },
   {
     id: 2,
     image: require('./images/tagu2.png'),
     title: 'TAGU Polska',
     text: 'Reklama ulubionych kominków elektrycznych w Europie',
-    link: 'https://shorturl.at/copHI'
+    link: 'https://www.instagram.com/reel/CmI8ff_DUM2/?igshid=MDJmNzVkMjY%3D'
   },
   {
     id: 3,
     image: require('./images/topshop.png'),
     title: 'Topeshop',
     text: 'Współpraca z producentem mebli nowoczesnych',
-    link: 'https://shorturl.at/svxC9'
+    link: 'https://www.instagram.com/reel/Ch6cgM1jTAq/?igshid=YmMyMTA2M2Y%3D'
   },
   {
     id: 4,
     image: require('./images/biostar.png'),
     title: 'BioStar',
     text: ' Reklama wysokiej klasy naturalnych środków czystości:',
-    link: 'https://shorturl.at/dsvw5'
+    link: 'https://www.instagram.com/reel/Ce0LBcfjaUq/?igshid=YmMyMTA2M2Y%3D'
   },
   {
     id: 5,
     image: require('./images/opti.png'),
     title: 'Optisofa',
     text: 'Skandynawskie sofy z funkcją spania, narożniki, fotele',
-    link: 'https://shorturl.at/vFY59'
+    link: 'https://www.instagram.com/reel/Cru3QDXoVZa/?igshid=YmMyMTA2M2Y%3D'
   },
   {
     id: 6,
